f53f96090feb8195494fc48bcf2b41f269b975af
Anshul <kumaranshul8439@gmail.com>
2021-05-26 23:34:50 +0530

    message order resolved

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,6 @@ import 'screens/welcome_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/registration_screen.dart';
 import 'screens/chat_screen.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
